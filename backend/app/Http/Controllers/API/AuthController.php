858b807269bc3b81376ef503f47e563a4ced481c
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-04-01 22:52:44 +0800

    fix usertype error

--- backend/app/Http/Controllers/API/AuthController.php
+++ backend/app/Http/Controllers/API/AuthController.php
@@ -74,7 +74,7 @@ class AuthController extends Controller
             'address' => $request->address,
             'contact_number' => $request->contact_number,
             'gender' => $request->gender,
-            'usertype' => $request->usertype,
+            'usertype' => $request->usertype != null ? $request->usertype : 1,
             'userstatus' => 1
         ]);
 
