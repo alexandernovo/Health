837565bb260290dce3e84126680021308bd1655f
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-04-10 20:39:46 +0800

    add seeder

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * php artisan db:seed --class=AdminSeeder
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'firstname' => 'Admin',
+                'lastname' => 'Admin',
+                'username' => 'admin',
+                'password' => Hash::make('Admin'),
+                'usertype' => 0,
+                'birthdate' => '1990-01-01',
+                'education' => 'Bachelor\'s Degree',
+                'occupation' => 'Administrator',
+                'contact_number' => '1234567890',
+                'address' => 'Admin Address',
+                'gender' => 'Male',
+                'religion' => 'Roman Catholic',
+                'userstatus' => 1,
+            ]
+        ]);
+    }
+}
