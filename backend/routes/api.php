83052ce44df2528ecf595d79a24b6d4295a186ed
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-04-12 22:26:53 +0800

    add new api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\ConsultationTypeController;
 use App\Http\Controllers\API\AppointmentController;
 use App\Http\Controllers\API\MaternalRecordController;
+use App\Http\Controllers\API\RecordController;
 
 Route::prefix('users')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -14,6 +15,7 @@ Route::prefix('users')->group(function () {
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/getUsers', [UserController::class, 'getUsers']);
     Route::get('/getPatients', [UserController::class, 'getPatients']);
+    Route::get('/getUserById/{id}', [UserController::class, 'getUserById']);
     Route::put('/updateUser/{id}', [UserController::class, 'updateUser']);
     Route::put('/activation/{id}/{status}', [UserController::class, 'activation']);
 });
@@ -30,15 +32,23 @@ Route::prefix('appointment')->group(function () {
     Route::post('/createappointment', [AppointmentController::class, 'createAppointment']);
     Route::get('/getAppointments/{status}', [AppointmentController::class, 'getAppointments']);
     Route::get('/getAppointmentById/{id}', [AppointmentController::class, 'getAppointmentById']);
+    Route::post('/changeAppointmentStatus/{id}/{status}', [AppointmentController::class, 'changeAppointmentStatus']);
 });
 
 Route::prefix('maternal')->group(function () {
     Route::post('/createMaternalRecord', [MaternalRecordController::class, 'createMaternalRecord']);
 });
 
+Route::prefix('record')->group(function () {
+    Route::get('/getUserWithRecord', [RecordController::class, 'getUserWithRecord']);
+    Route::get('/getUserRecord/{id}', [RecordController::class, 'getUserRecord']);
+});
 
 
-//remove routes
-// php artisan route:list | Where-Object { $_ -notmatch "/api" } 
-//add routes
-// php artisan route:list --path=api
+
+
+//  remove routes
+//  php artisan route:list | Where-Object { $_ -notmatch "/api" } 
+//  add routes
+//  after you add routes add this command
+//  php artisan route:list --path=api
