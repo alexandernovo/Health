89d1c31b6a79674165cd884f05a02935b005eb34
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-04-28 22:48:23 +0800

    new types

---
+++ frontend/src/types/familyAssessment.ts
@@ -0,0 +1,23 @@
+export interface FamilyAssessmentModel {
+    keyId?: string;
+    familyAssessmentId?: number,
+    familyId?: number,
+    dateOfVisit?: string,
+    methodAccepted?: string,
+    nameAndSignatureSP?: string,
+    dateFollowUp?: string,
+    medicalFindings?: string
+}
+
+export const FamilyAssessmentModelInitialValue = () => {
+    return {
+        keyId: "",
+        familyAssessmentId: 0,
+        familyId: 0,
+        dateOfVisit: "",
+        methodAccepted: "",
+        nameAndSignatureSP: "",
+        dateFollowUp: "",
+        medicalFindings: ""
+    }
+}
