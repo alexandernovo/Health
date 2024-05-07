918aaae2423068c0fdcd8156a6bbe9038d500b2f
Alexander0906 <alexander.novo@fgciph.com>
2024-05-07 11:04:27 +0800

    add new model and relationship

--- backend/app/Models/Immunization.php
+++ backend/app/Models/Immunization.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\ImmunizationResult;
 
 class Immunization extends Model
 {
@@ -65,5 +66,10 @@ class Immunization extends Model
         'deworming10To19YO',
         'ferrousSulfate'
     ];
+
+    public function immunization_result(): HasMany
+    {
+        return $this->hasMany(ImmunizationResult::class, 'immunizationId');
+    }
     
 }
