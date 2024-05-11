68ae1de64d998b1ab22414eb016e7680283f0b60
Alexander0906 <alexander.novo@fgciph.com>
2024-05-11 11:20:50 +0800

    complete the function of logout

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { removeUser } from '@store/user/userSlice';
+
 const Settings: React.FC = () => {
+    const token: string | null = localStorage.getItem("token");
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const Logout = async () => {
+        const response = await axios.post("/api/users/logout", {},
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+
+        if (response.data.status == "success") {
+            localStorage.removeItem("token");
+            dispatch(removeUser());
+            navigate("/");
+        }
+    }
+
     return (
         <div>
             <div className='flex justify-center p-4 px-5 pt-5'>
@@ -19,6 +44,14 @@ const Settings: React.FC = () => {
                         </svg>
                         ACCOUNT SETTINGS
                     </Link>
+                </div>
+                <div className='w-[30%] border mx-auto rounded-lg  shadow-md bg-white p-3 mt-3'>
+                    <button onClick={() => Logout()} className='w-full h-full text-[14px] flex items-center gap-2 font-semibold'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                        </svg>
+                        LOGOUT
+                    </button>
                 </div>
             </div>
         </div>
