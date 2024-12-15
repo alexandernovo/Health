ff02c887c10c433f34079f7fb47b94ce9176105e
Alexander Novo <alexandernovo84@gmail.com>
2024-12-15 21:58:19 +0800

    updates

--- frontend/src/pages/Appointments.tsx
+++ frontend/src/pages/Appointments.tsx
@@ -192,7 +192,7 @@ const Appointments: React.FC = () => {
                     (
                         <>
                             {
-                                row.appointmentStatus != 2 && (
+                                row.appointmentStatus != 2 && !(user.usertype == 1 && row.appointmentStatus == 3) && (
                                     <Link to={ToRedirect(row.consultationTypeName, row.appointment_id, row.appointmentStatus)} className=' btn btn-primary btn-xs px-3 text-white btn-outline active:text-white hover:text-white  text-[13px]'>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
