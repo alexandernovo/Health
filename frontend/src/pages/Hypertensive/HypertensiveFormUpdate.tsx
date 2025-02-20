f6d5d1b92720b823592f040ef3d3485e1f359a3a
Alexander Novo <alexandernovo84@gmail.com>
2025-02-20 20:38:04 +0800

    update

--- frontend/src/pages/Hypertensive/HypertensiveFormUpdate.tsx
+++ frontend/src/pages/Hypertensive/HypertensiveFormUpdate.tsx
@@ -101,7 +101,7 @@ const HypertensiveFormUpdate: React.FC = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
                             </svg>
-                            {appointment?.firstname} ${getMiddleInitial(appointment?.middlename || '')} {appointment?.lastname} ${appointment?.extension || ''} Records
+                            {appointment?.firstname} {getMiddleInitial(appointment?.middlename || '')} {appointment?.lastname} {appointment?.extension || ''} Records
                         </Link>
                     </li>
                     <li>
@@ -109,7 +109,7 @@ const HypertensiveFormUpdate: React.FC = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
                             </svg>
-                            {appointment?.firstname} ${getMiddleInitial(appointment?.middlename || '')} {appointment?.lastname} ${appointment?.extension || ''} Hypertensive/Diabetic Records
+                            {appointment?.firstname} {getMiddleInitial(appointment?.middlename || '')} {appointment?.lastname} {appointment?.extension || ''} Hypertensive/Diabetic Records
                         </Link>
                     </li>
                     <li>
@@ -118,7 +118,7 @@ const HypertensiveFormUpdate: React.FC = () => {
                                 <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
                                 <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
                             </svg>
-                            Update {appointment?.firstname} ${getMiddleInitial(appointment?.middlename || '')} {appointment?.lastname} ${appointment?.extension || ''} Record
+                            Update {appointment?.firstname} {getMiddleInitial(appointment?.middlename || '')} {appointment?.lastname} {appointment?.extension || ''} Record
                         </span>
                     </li>
                 </ul>
